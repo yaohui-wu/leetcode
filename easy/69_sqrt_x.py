f8e9b73b58567e3a3daca28c3af1d071461cffd7
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-17 10:16:42 -0500

    Solve 69. Sqrt(x)

--- easy/69_sqrt_x.py
+++ easy/69_sqrt_x.py
@@ -1,3 +1,16 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        
+        if x == 0 or x == 1:
+            return x
+        left = 0
+        right = x
+        while left < right:
+            mid = left + (right - left) // 2
+            square = mid * mid
+            if square < x:
+                left = mid
+            elif square > x:
+                right = mid
+            else:
+                return mid
+        return left
