73698357dc915243bf2dc021bb86404b7f54da63
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-16 19:21:02 -0500

    Solve 20. Valid Parentheses

--- easy/20_valid_parentheses.py
+++ easy/20_valid_parentheses.py
@@ -3,8 +3,13 @@ class Solution:
         open_close = {"(": ")", "{": "}", "[": "]"}
         stack = []
         for c in s:
+            # If C is an open bracket, push the corresponding close bracket to
+            # the stack.
             if c in open_close:
                 stack.append(open_close.get(c))
+            # If C is a close bracket, check if the stack is empty or the top
+            # of the stack is the same close bracket.
             elif len(stack) == 0 or stack.pop() != c:
                 return False
+        # If the stack is empty, then all the brackets are balanced.
         return len(stack) == 0
