10e01be3afd5e0f2d8f1f4473021ac6c8b8e265a
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-16 19:07:14 -0500

    Solve 20. Valid Parentheses

--- easy/20_valid_parentheses.py
+++ easy/20_valid_parentheses.py
@@ -1,12 +1,10 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        close_open = {")": "(", "}": "{", "]": "["}
+        open_close = {"(": ")", "{": "}", "[": "]"}
         stack = []
         for c in s:
-            if c in close_open.values():
-                stack.append(c)
-            elif len(stack) == 0:
-                return False
-            elif stack.pop() != close_open.get(c):
+            if c in open_close:
+                stack.append(open_close.get(c))
+            elif len(stack) == 0 or stack.pop() != c:
                 return False
         return len(stack) == 0
