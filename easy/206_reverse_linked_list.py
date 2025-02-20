df869c421fc4efc5e2f1660b61ada7af862e7223
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-20 13:54:42 -0500

    Solve 206. Reverse Linked List

--- easy/206_reverse_linked_list.py
+++ easy/206_reverse_linked_list.py
@@ -5,4 +5,11 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
+        curr = None
+        prev = head
+        while prev is not None:
+            next = curr
+            curr = prev
+            prev = prev.next
+            curr.next = next
+        return curr
