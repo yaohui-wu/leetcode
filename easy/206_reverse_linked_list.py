156e3b6200d5bb504cec42adf92c8e5b56e0f63e
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-21 14:46:36 -0500

    Solve 206. Reverse Linked List

--- easy/206_reverse_linked_list.py
+++ easy/206_reverse_linked_list.py
@@ -5,10 +5,16 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Head of the reversed linked list.
         curr = None
+        # Iterate through the original linked list.
         while head is not None:
             next = curr
+            # The current node of the original linked list is the new head of
+            # the reversed linked list.
             curr = head
+            # Move the head of the original linked list.
             head = head.next
+            # Attach the rest of the reversed linked list.
             curr.next = next
         return curr
