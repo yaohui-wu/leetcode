5d8366ca2b177d4abf9b9db7bf9a1a7694e5f495
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-02-23 12:14:39 -0500

    Solve 2. Add Two Numbers

--- easy/0002_add_two_numbers.py
+++ easy/0002_add_two_numbers.py
@@ -5,4 +5,26 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        
+        head = ListNode(None)
+        curr = head
+        carry = False
+        while l1 is not None or l2 is not None:
+            sum = 0
+            if l1 is not None:
+                sum += l1.val
+                l1 = l1.next
+            if l2 is not None:
+                sum += l2.val
+                l2 = l2.next
+            if carry:
+                sum += 1
+                carry = False
+            if sum >= 10:
+                carry = True
+                sum %= 10
+            curr.next = ListNode(sum)
+            curr = curr.next
+        if carry:
+            curr.next = ListNode(1)
+        head = head.next
+        return head
