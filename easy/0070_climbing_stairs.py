3f04b438438d5785f6575df1ac8450c94278d55a
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-19 14:02:39 -0400

    Solve 70. Climbing Stairs

--- easy/0070_climbing_stairs.py
+++ easy/0070_climbing_stairs.py
@@ -1,9 +1,15 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        # Base cases.
         if n == 1 or n == 2:
             return n
         # Dynamic programming.
-        steps = [1, 2]
-        for i in range(2, n):
-            steps.append(steps[i - 1] + steps[i - 2])
-        return steps[n - 1]
+        prev_two = 1 # Ways to reach previous two steps.
+        prev = 2 # Ways to reach previous step.
+        total = 3 # Ways to reach current step.
+        for _ in range (3, n):
+            # Move one step and update the values.
+            prev_two = prev
+            prev = total
+            total = prev + prev_two
+        return total
