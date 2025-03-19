53e6c71c5a4406e8f364f13352c061905c10bcd4
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-19 10:53:09 -0400

    Solve 9. Palindrome Number

--- easy/0009_palindrome_number.py
+++ easy/0009_palindrome_number.py
@@ -1,3 +1,16 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        
+        # Negative numbers cannot be palindromes.
+        if x < 0:
+            return False
+        # Single digit numbers are palindromes.
+        if x < 10:
+            return True
+        # Reverse the number and check if it is the same as the original.
+        reverse = x % 10
+        num = x // 10
+        while num > 0:
+            reverse *= 10
+            reverse += num % 10
+            num //= 10
+        return reverse == x
