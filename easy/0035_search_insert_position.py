726a313be0ce2a701710e2ac103a6158d180b8e6
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-19 12:34:59 -0400

    Solve 35. Search Insert Position

--- easy/0035_search_insert_position.py
+++ easy/0035_search_insert_position.py
@@ -1,3 +1,25 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        
+        left = 0
+        # Target is less than the least element.
+        if nums[left] > target:
+            return 0
+        right = len(nums) - 1
+        # Target is greater than the greatest element.
+        if nums[right] < target:
+            return right + 1
+        # Binary search.
+        while left <= right:
+            mid = left + (right - left) // 2 # Avoid overflow.
+            num = nums[mid]
+            if num < target:
+                # Search right half.
+                left = mid + 1
+            elif num > target:
+                # Search left half.
+                right = mid - 1
+            else:
+                # Found the target.
+                return mid
+        # Return the index after the greatest element less than the target.
+        return right + 1
