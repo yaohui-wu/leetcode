2a7030f945f73d93a05e15470816dee51dde1636
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-19 11:32:52 -0400

    Solve 9. Palindrome Number

--- easy/0009_palindrome_number.py
+++ easy/0009_palindrome_number.py
@@ -6,15 +6,13 @@ class Solution:
         # Single digit numbers are palindromes.
         if x < 10:
             return True
-        reverse = x % 10
         # Positive numbers ending with 0 cannot be palindromes.
-        if reverse == 0:
+        if x % 10 == 0:
             return False
         # Reverse half of the number and compare it with the other half.
-        x //= 10
+        reverse = 0
         while reverse < x:
-            reverse *= 10
-            reverse += x % 10
+            reverse = reverse * 10 + x % 10
             x //= 10
         # Remove the middle digit if the number has an odd number of digits.
-        return reverse == x or reverse // 10 == x
+        return x == reverse or x == reverse // 10
