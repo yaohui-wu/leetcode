d2bc58787153ed18672beeef7fbd00ce6917ec15
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-19 15:25:45 -0400

    Solve 7. Reverse Integer

--- medium/0007_reverse_integer.py
+++ medium/0007_reverse_integer.py
@@ -1,3 +1,16 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        
+        limit = 2 ** 31 - 1 # Maximum signed 32-bit integer.
+        signed = False
+        if x < 0:
+            signed = True
+        x = abs(x)
+        reverse = 0
+        while x > 0:
+            reverse = reverse * 10 + x % 10
+            x //= 10
+        if reverse > limit:
+            return 0
+        if signed:
+            return -reverse
+        return reverse
