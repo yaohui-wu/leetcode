01389336fc8002bda1960bf6e4f0b731f3941903
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-20 11:50:44 -0400

    Solve 169. Majority Element

--- easy/0169_majority_element.py
+++ easy/0169_majority_element.py
@@ -1,9 +1,9 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        majority = len(nums) // 2
-        frequency = {}
+        majority = len(nums) // 2 # Majority threshold.
+        frequencies = {} # Frequency of each element.
         for i in nums:
-            frequency[i] = frequency.get(i, 0) + 1
-            if frequency[i] > majority:
+            frequencies[i] = frequencies.get(i, 0) + 1
+            if frequencies[i] > majority:
                 return i
         return -1
