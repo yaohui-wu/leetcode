5c5adaa84531e88fe2204d130253fe3aa8849d59
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-21 12:24:34 -0400

    Solve 217. Contains Duplicate

--- easy/0217_contains_duplicate.py
+++ easy/0217_contains_duplicate.py
@@ -1,3 +1,8 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        
+        distinct_nums = set()
+        for i in nums:
+            if i in distinct_nums:
+                return True
+            distinct_nums.add(i)
+        return False
