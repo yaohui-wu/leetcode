cf05c0c9ee12b47c3809ddf4f4dd873dafa474d9
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-21 16:21:45 -0400

    Solve 283. Move Zeroes

--- easy/0283_move_zeroes.py
+++ easy/0283_move_zeroes.py
@@ -3,4 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        
+        length = len(nums)
+        if length == 0 or length == 1:
+            return
+        zero = -1
+        found = False
+        i = 0
+        while not found and i < length:
+            if nums[i] == 0:
+                found = True
+                zero = i
+            i += 1
+        if not found:
+            return nums
+        next_zero = -1
