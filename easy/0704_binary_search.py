f4e17d3e55201d06cd9865f4ada32640c447cbab
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-21 12:30:40 -0400

    Solve 704. Binary Search

--- easy/0704_binary_search.py
+++ easy/0704_binary_search.py
@@ -1,3 +1,19 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        
+        # Binary search.
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2 # Prevent overflow.
+            num = nums[mid]
+            if num < target:
+                # Search the right half.
+                left = mid + 1
+            elif num > target:
+                # Search the left half.
+                right = mid - 1
+            else:
+                # Found the target.
+                return mid
+        # Target not found.
+        return -1
