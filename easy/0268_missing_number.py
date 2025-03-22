54aeb316c86b6b6f45944fe1d8d13d52ec162eb2
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-22 14:43:56 -0400

    Solve 268. Missing Number

--- easy/0268_missing_number.py
+++ easy/0268_missing_number.py
@@ -1,3 +1,9 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        
+        nums_set = set()
+        for i in nums:
+            nums_set.add(i)
+        for i in range(len(nums) + 1):
+            if i not in nums_set:
+                return i
+        return -1
