cac7b5ecca7766977c579d8ec5056c6c28c7dd00
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-22 15:51:16 -0400

    Solve 202. Happy Number

--- easy/0202_happy_number.py
+++ easy/0202_happy_number.py
@@ -1,3 +1,17 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        
+        nums = set()
+        num = n
+        while n not in nums:
+            sum = 0
+            while num > 0:
+                digit = num % 10
+                sum += digit * digit
+                num //= 10
+            if sum == 1:
+                return True
+            if sum in nums:
+                return False
+            nums.add(sum)
+            num = sum
+        return False
