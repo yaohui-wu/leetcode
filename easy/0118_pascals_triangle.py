9c45f0fb4de84ae88c972726838ff5d88123d79b
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-22 11:32:03 -0400

    Solve 118. Pascal's Triangle

--- easy/0118_pascals_triangle.py
+++ easy/0118_pascals_triangle.py
@@ -1,3 +1,9 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        
+        pascals = [[1] * i for i in range(1, numRows + 1)]
+        for i in range(2, numRows):
+            for j in range(i + 1):
+                if j != 0 and j != i:
+                    num = pascals[i - 1][j - 1] + pascals[i - 1][j]
+                    pascals[i][j] = num
+        return pascals
