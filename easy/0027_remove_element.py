7b8343db77e9216401d077b63bbd068b8b87eeb6
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-22 10:24:49 -0400

    Solve 27. Remove Element

--- easy/0027_remove_element.py
+++ easy/0027_remove_element.py
@@ -1,3 +1,9 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        
+        length = len(nums)
+        k = 0
+        for i in range(length):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
