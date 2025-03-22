a0f674adf9bbb71cfcd020a2902dc3f4699b6e30
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-21 22:44:39 -0400

    Solve 88. Merge Sorted Array

--- easy/0088_merge_sorted_array.py
+++ easy/0088_merge_sorted_array.py
@@ -3,4 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        
+        i = m + n - 1
+        while m > 0 and n > 0:
+            num1 = nums1[m - 1]
+            num2 = nums2[n - 1]
+            if num1 < num2:
+                nums1[i] = num2
+                n -= 1
+            else:
+                nums1[i] = num1
+                m -= 1
+            i -= 1
+        for i in range(n):
+            nums1[i] = nums2[i]
