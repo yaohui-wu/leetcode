d6b29f44d1b3565f210d14732d337d979c5c0875
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-21 21:24:48 -0400

    Solve 283. Move Zeroes

--- easy/0283_move_zeroes.py
+++ easy/0283_move_zeroes.py
@@ -6,17 +6,18 @@ class Solution:
         length = len(nums)
         if length == 0 or length == 1:
             return
-        zero = -1
+        zero = -1 # Index of the current first zero element.
         i = 0
         while zero == -1 and i < length:
             if nums[i] == 0:
                 zero = i
             i += 1
         if zero == -1:
+            # No zero found.
             return nums
         for i in range(zero + 1, length):
-            num = nums[i]
-            if num != 0:
-                nums[zero] = num
+            # Find the next non-zero element and swap it with the zero.
+            if nums[i] != 0:
+                nums[zero] = nums[i]
                 nums[i] = 0
                 zero += 1
