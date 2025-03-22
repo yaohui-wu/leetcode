c7e41217815a18e15dd384459562b7d46b7235be
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-22 14:17:05 -0400

    Solve 283. Move Zeroes

--- easy/0283_move_zeroes.py
+++ easy/0283_move_zeroes.py
@@ -3,15 +3,12 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        length = len(nums)
-        if length == 0 or length == 1:
-            return
-        zeroes = 0 # Size of the current window of zeroes.
-        for i in range(length):
+        size = 0 # Size of the current window of zeroes.
+        for i in range(len(nums)):
             if nums[i] == 0:
-                zeroes += 1 # Expand the window.
-            elif zeroes > 0:
+                size += 1 # Expand the window.
+            elif size > 0:
                 # Swap the current non-zero element with the first zero in the
                 # window.
-                nums[i - zeroes] = nums[i]
+                nums[i - size] = nums[i]
                 nums[i] = 0
