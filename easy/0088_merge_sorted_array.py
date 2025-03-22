6cf12ed270551f7b6031a8af84039f7fd4b872f3
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-22 14:09:49 -0400

    Solve 88. Merge Sorted Array

--- easy/0088_merge_sorted_array.py
+++ easy/0088_merge_sorted_array.py
@@ -3,8 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        i = m + n - 1
+        i = m + n - 1 # Current index to insert.
+        # Iterate from the end of both arrays.
         while m > 0 and n > 0:
+            # Insert the lesser number to the end of nums1.
             num1 = nums1[m - 1]
             num2 = nums2[n - 1]
             if num1 < num2:
@@ -14,5 +16,6 @@ class Solution:
                 nums1[i] = num1
                 m -= 1
             i -= 1
+        # Copy the remaining elements of nums2 to the front of nums1.
         for i in range(n):
             nums1[i] = nums2[i]
