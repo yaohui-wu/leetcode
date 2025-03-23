0215dbcb7a1866addd0e4064565893c3c918b08e
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-22 21:00:21 -0400

    Solve 278. First Bad Version

--- easy/0278_first_bad_version.py
+++ easy/0278_first_bad_version.py
@@ -3,4 +3,13 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        
+        # Binary search.
+        left = 1
+        right = n
+        while left <= right:
+            mid = left + (right - left) // 2
+            if isBadVersion(mid):
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left
