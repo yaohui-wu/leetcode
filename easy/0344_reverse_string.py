731607867e8301781463825516db5a6b2b697a60
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-22 20:32:56 -0400

    Solve 344. Reverse String

--- easy/0344_reverse_string.py
+++ easy/0344_reverse_string.py
@@ -3,4 +3,8 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        
+        length = len(s)
+        for i in range(length // 2):
+            temp = s[i]
+            s[i] = s[length - 1 - i]
+            s[length - 1 - i] = temp
