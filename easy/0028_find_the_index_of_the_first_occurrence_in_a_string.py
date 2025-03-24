19dbd9fc8435a9ac3f6516dfb33c28ab6bdb6aaa
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-24 16:43:23 -0400

    Solve 28. Find the Index of the First Occurrence in a String

--- easy/0028_find_the_index_of_the_first_occurrence_in_a_string.py
+++ easy/0028_find_the_index_of_the_first_occurrence_in_a_string.py
@@ -2,19 +2,14 @@ class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         needle_len = len(needle)
         haystack_len = len(haystack)
-        diff = haystack_len - needle_len
-        if diff < 0:
+        # Needle is longer than haystack.
+        if needle_len > haystack_len:
             return -1
-        left = 0
-        right = needle_len - 1
-        while right < haystack_len:
-            i = needle_len - 1
-            j = right
-            while i >= 0 and needle[i] == haystack[j]:
-                i -= 1
-                j -= 1
-            if i == -1:
-                return left
-            left += 1
-            right += 1
+        # Empty needle.
+        elif needle_len == 0:
+            return 0
+        # Search for the needle in the haystack.
+        for i in range(haystack_len - needle_len + 1):
+            if haystack[i:i + needle_len] == needle:
+                return i
         return -1
