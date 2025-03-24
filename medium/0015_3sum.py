6e3c217e74006f46cba170da0786c982d8653033
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-24 13:35:41 -0400

    Solve 15. 3Sum

--- medium/0015_3sum.py
+++ medium/0015_3sum.py
@@ -1,14 +1,20 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        solutions = []
+        solution = []
+        nums.sort()
         length = len(nums)
+        if length < 3 or nums[0] > 0:
+            return solution
+        num_to_index = {}
         for i in range(length):
-            target = -nums[i]
-            num_to_index = {}
-            for j in range(length):
-                if i != j:
-                    complement = target - nums[j]
-                    if complement in num_to_index:
-                        solutions.append([nums[i], nums[j], complement])
-                    num_to_index[nums[j]] = j
-        return solutions
+            num_to_index[nums[i]] = i
+        i = 0
+        while i < length - 2 and nums[i] <= 0:
+            j = i + 1
+            while j < length - 1:
+                target = -(nums[i] + nums[j])
+                if target in num_to_index and num_to_index[target] > j:
+                    solution.append([target, nums[i], nums[j]])
+                j = num_to_index[nums[j]] + 1
+            i = num_to_index[nums[i]] + 1
+        return solution
