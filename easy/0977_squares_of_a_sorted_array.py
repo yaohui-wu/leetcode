ebbcbd50d2a9e41e4917b661b942b4454e55695a
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-24 19:22:38 -0400

    Solve 977. Squares of a Sorted Array

--- easy/0977_squares_of_a_sorted_array.py
+++ easy/0977_squares_of_a_sorted_array.py
@@ -8,20 +8,14 @@ class Solution:
         if nums[length - 1] < 0:
             nums.reverse()
             return [i * i for i in nums]
-        right = 1
-        while right < length and nums[right] < 0:
-            right += 1
-        sorted = []
-        left = right - 1
-        while left >= 0 and right < length:
-            if abs(nums[left]) < nums[right]:
-                sorted.append(nums[left])
-                left -= 1
+        sorted_squares =[0] * length
+        left = 0
+        right = length - 1
+        for i in range(length - 1, -1, -1):
+            if abs(nums[left]) > abs(nums[right]):
+                sorted_squares[i] = nums[left] * nums[left]
+                left += 1
             else:
-                sorted.append(nums[right])
-                right += 1
-        for i in range(left, -1, -1):
-            sorted.append(nums[i])
-        for i in range(right, length):
-            sorted.append(nums[i])
-        return [i * i for i in sorted]
+                sorted_squares[i] = nums[right] * nums[right]
+                right -= 1
+        return sorted_squares
