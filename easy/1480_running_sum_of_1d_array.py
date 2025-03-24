b45db8e2a530bba50f2f8505cabb688e62a8c0d5
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-24 19:56:24 -0400

    Solve 1480. Running Sum of 1d Array

--- easy/1480_running_sum_of_1d_array.py
+++ easy/1480_running_sum_of_1d_array.py
@@ -1,3 +1,9 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        
+        length = len(nums)
+        if length == 0:
+            return []
+        sums = [nums[0]]
+        for i in range(1, length):
+            sums.append(sums[i - 1] + nums[i])
+        return sums
