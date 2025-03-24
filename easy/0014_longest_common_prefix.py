9147ed9a77764a6c09ac463721ef2f7168ac54af
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-24 15:23:55 -0400

    Solve 14. Longest Common Prefix

--- easy/0014_longest_common_prefix.py
+++ easy/0014_longest_common_prefix.py
@@ -1,14 +1,16 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        lcp = "" # Longest common prefix.
+        # Empty list, return empty string.
+        if len(strs) == 0:
+            return ""
+        # Longest common prefix (LCP) cannot be longer than the shortest
+        # string.
         shortest_s = min(strs, key=len)
-        i = 0
-        length = len(shortest_s)
-        while i < length:
-            c = shortest_s[i]
+        for i in range(len(shortest_s)):
+            c = shortest_s[i] # Character to compare.
+            # Check if the character is the same in all strings.
             for s in strs:
                 if s[i] != c:
-                    return lcp
-            lcp += c
-            i += 1
-        return lcp
+                    # Mismatch found, return the LCP found so far.
+                    return shortest_s[:i]
+        return shortest_s
