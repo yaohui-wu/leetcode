c3b4fc2f8ba1d7dc0c7f1c7e692b0cf917e44b6f
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-24 17:29:35 -0400

    Solve 242. Valid Anagram

--- easy/0242_valid_anagram.py
+++ easy/0242_valid_anagram.py
@@ -3,16 +3,16 @@ class Solution:
         if len(s) != len(t):
             return False
         # Frequency of each letter (26 letters in the alphabet).
-        letters = [0] * 26
+        frequencies = [0] * 26
         first = ord("a") # ASCII value of 'a'.
         # Count the frequency of each letter in s.
         for c in s:
-            letters[ord(c) - first] += 1
+            frequencies[ord(c) - first] += 1
         # Subtract the frequency of each letter in t.
         for c in t:
-            letters[ord(c) - first] -= 1
+            frequencies[ord(c) - first] -= 1
         # If all the frequencies are 0, then s and t are anagrams.
-        for i in letters:
+        for i in frequencies:
             if i != 0:
                 return False
         return True
