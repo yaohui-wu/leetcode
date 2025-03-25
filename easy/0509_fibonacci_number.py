94cf44c43bf4b9cdcf9588de2a80a89d21983fb0
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-24 20:12:02 -0400

    Solve 509. Fibonacci Number

--- easy/0509_fibonacci_number.py
+++ easy/0509_fibonacci_number.py
@@ -1,3 +1,13 @@
 class Solution:
     def fib(self, n: int) -> int:
-        
+        # # Base cases: F(0) = 0 and F(1) = 1.
+        if n == 0 or n == 1:
+            return n
+        second_last = 0 # F(n-2).
+        last = 1 # F(n-1).
+        for _ in range(2, n + 1):
+            current = last + second_last # F(n) = F(n-1) + F(n-2).
+            second_last = last # Move F(n-2) forward.
+            last = current # Move F(n-1) forward.
+        # Return F(n).
+        return last
