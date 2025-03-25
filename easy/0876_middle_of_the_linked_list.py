ec5e9edd28c1ec5aeb91176e004544c35dc7110b
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-25 17:51:36 -0400

    Solve 876. Middle of the Linked List

--- easy/0876_middle_of_the_linked_list.py
+++ easy/0876_middle_of_the_linked_list.py
@@ -5,11 +5,10 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        size = 0
         middle = head
-        while head is not None:
-            size += 1
-            head = head.next
-        for _ in range(size // 2):
-            middle = middle.next
+        while head is not None and head.next is not None:
+            middle = middle.next # Move one step.
+            head = head.next.next # Move two steps.
+        # If length of linked list is odd, middle will be the exact middle.
+        # If length of linked list is even, middle will be the second middle.
         return middle
