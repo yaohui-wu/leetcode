e71d33c24390008cc6104535f76afdb3f8b8a865
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-25 18:10:34 -0400

    Solve 141. Linked List Cycle

--- easy/0141_linked_list_cycle.py
+++ easy/0141_linked_list_cycle.py
@@ -6,4 +6,12 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        
+        slow = head
+        fast = head
+        while fast is not None and fast.next is not None:
+            slow = slow.next # Move one step.
+            fast = fast.next.next # Move two steps.
+            # If there is a cycle, the two pointers will meet.
+            if slow == fast:
+                return True
+        return False
