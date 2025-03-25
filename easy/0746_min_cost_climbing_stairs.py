d8cbbb185730a1589d200d76281a84e9d71ac6ef
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-24 21:39:08 -0400

    Solve 746. Min Cost Climbing Stairs

--- easy/0746_min_cost_climbing_stairs.py
+++ easy/0746_min_cost_climbing_stairs.py
@@ -1,3 +1,9 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        
+        second_last = cost[0]
+        last = cost[1]
+        for i in range(2, len(cost)):
+            min_cost = min(second_last, last) + cost[i]
+            second_last = last
+            last = min_cost
+        return min_cost
