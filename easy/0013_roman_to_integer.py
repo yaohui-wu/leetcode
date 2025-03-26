d1608cd548e28c4d275756cf4b761611c05368d3
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-25 21:14:37 -0400

    Solve 13. Roman to Integer

--- easy/0013_roman_to_integer.py
+++ easy/0013_roman_to_integer.py
@@ -1,3 +1,16 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        
+        # Map the roman numeral to integer.
+        roman_to_int = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500,
+                        "M": 1_000}
+        length = len(s)
+        num = roman_to_int[s[length - 1]] # Last numeral.
+        for i in range(length - 1):
+            current = roman_to_int[s[i]]
+            next = roman_to_int[s[i + 1]]
+            # Subtract if smaller numeral precedes larger one, else add.
+            if current < next:
+                num -= current
+            else:
+                num += current
+        return num
