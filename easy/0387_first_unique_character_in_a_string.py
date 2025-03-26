664d1a71069a9ead29e0845eb595edeafb0c5d07
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-26 16:04:56 -0400

    Solve 387. First Unique Character in a String

--- easy/0387_first_unique_character_in_a_string.py
+++ easy/0387_first_unique_character_in_a_string.py
@@ -1,3 +1,18 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        
+        length = len(s)
+        index = length
+        char_to_entry = {} # char -> [count, index].
+        for i in range(length):
+            if s[i] in char_to_entry:
+                entry = char_to_entry[s[i]]
+                entry[0] += 1
+                entry[1] = i
+            else:
+                char_to_entry[s[i]] = [1, i]
+        for entry in char_to_entry.values():
+            if entry[0] == 1:
+                index = min(index, entry[1])
+        if index == length:
+            return -1
+        return index
