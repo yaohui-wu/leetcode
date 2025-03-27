1cc80029d9f34ab7e42343b66171c247f87d5f9e
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-26 21:30:17 -0400

    Solve 160. Intersection of Two Linked Lists

--- easy/0160_intersection_of_two_linked_lists.py
+++ easy/0160_intersection_of_two_linked_lists.py
@@ -6,4 +6,20 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        
+        if headA is None or headB is None:
+            return None
+        a = headA
+        b = headB
+        while a != b:
+            # If a or b reaches the end of the list, reset it to the head of
+            # the other list.
+            if a is None:
+                a = headB
+            else:
+                a = a.next
+            if b is None:
+                b = headA
+            else:
+                b = b.next
+        # a and b are either the same node or both are None.
+        return a
