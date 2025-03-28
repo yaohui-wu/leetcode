a99da632af6c063fa2e8effdf129a31c92509f2a
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-27 22:05:33 -0400

    Solve 234. Palindrome Linked List

--- easy/0234_palindrome_linked_list.py
+++ easy/0234_palindrome_linked_list.py
@@ -5,4 +5,26 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        
+        # Find the middle of the linked list.
+        slow = head
+        fast = head
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+        # Reverse the second half of the linked list.
+        prev = slow
+        slow = slow.next
+        prev.next = None
+        while slow is not None:
+            next = prev
+            prev = slow
+            slow = slow.next
+            prev.next = next
+        # Compare the first half and the reversed second half.
+        curr = prev
+        while curr is not None:
+            if curr.val != head.val:
+                return False
+            curr = curr.next
+            head = head.next
+        return True
