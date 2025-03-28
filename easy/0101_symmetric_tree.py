24b8afa58b3bfaa926cee6bcd3bbda45e6e5ea38
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-27 21:26:11 -0400

    Solve 101. Symmetric Tree

--- easy/0101_symmetric_tree.py
+++ easy/0101_symmetric_tree.py
@@ -6,4 +6,16 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        
+        # Empty tree is trivially symmetric.
+        # Check if the the left and right subtrees are mirrors of each other.
+        return root is None or self.isMirror(root.left, root.right)
+    
+    def isMirror(self, left, right):
+        # If either of the subtrees is None, they are symmetric only if both
+        # are None.
+        if left is None or right is None:
+            return left == right
+        # Check if the current nodes are equal and their subtrees are mirrors.
+        return (left.val == right.val
+                and self.isMirror(left.left, right.right)
+                and self.isMirror(left.right, right.left))
