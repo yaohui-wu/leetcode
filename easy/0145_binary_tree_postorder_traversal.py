813d99fdb0eff2fb4b9722837f9e6587b034796b
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-28 10:31:42 -0400

    Solve 145. Binary Tree Postorder Traversal

--- easy/0145_binary_tree_postorder_traversal.py
+++ easy/0145_binary_tree_postorder_traversal.py
@@ -6,4 +6,11 @@
 #         self.right = right
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        
+        # Empty tree.
+        if root is None:
+            return []
+        # Postorder traversal: left -> right -> root.
+        postorder = self.postorderTraversal(root.left)
+        postorder += self.postorderTraversal(root.right)
+        postorder.append(root.val)
+        return postorder
