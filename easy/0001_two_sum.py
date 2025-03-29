b1481b2132ed6eac1b63deb7d929f7c317a7c303
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-29 12:14:17 -0400

    Solve 1. Two Sum

--- easy/0001_two_sum.py
+++ easy/0001_two_sum.py
@@ -1,13 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_to_index = {} # Map the integer in the list to its index.
+        num_to_index = {} # Map the element in the list to its index.
         for i in range(len(nums)):
             complement = target - nums[i]
             # Complement is found in elements before the current element.
             if complement in num_to_index:
                 # Return the index of the complement and the current element.
                 return [num_to_index[complement], i]
-            # Add the current element to the hash table.
+            # Add the current element to the hash map.
             num_to_index[nums[i]] = i
-        # Renturn an empty list if no solution is found.
-        return []
+        # No solution found.
+        return [-1, -1]
