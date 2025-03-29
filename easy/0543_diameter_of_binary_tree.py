449379d70cefc8d768a92fe831ae3965e1e8ca3c
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-29 12:09:44 -0400

    Solve 543. Diameter of Binary Tree

--- easy/0543_diameter_of_binary_tree.py
+++ easy/0543_diameter_of_binary_tree.py
@@ -5,5 +5,19 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    diameter = 0
+
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        
+        self.max_depth(root)
+        return self.diameter
+    
+    def max_depth(self, root):
+        # Empty tree.
+        if root is None:
+            return 0
+        left_depth = self.max_depth(root.left) # Depth of left subtree.
+        right_depth = self.max_depth(root.right) # Depth of right subtree.
+        # Update the diameter if the path through the root is longer.
+        self.diameter = max(self.diameter, left_depth + right_depth)
+        # Return the maximum depth of the tree.
+        return max(left_depth, right_depth) + 1
