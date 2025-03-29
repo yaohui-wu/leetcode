5437d3e2b8d6623ba6fbb6fc03e1716510d23ca8
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-29 17:52:38 -0400

    Solve 111. Minimum Depth of Binary Tree

--- easy/0111_minimum_depth_of_binary_tree.py
+++ easy/0111_minimum_depth_of_binary_tree.py
@@ -6,4 +6,18 @@
 #         self.right = right
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
-        
+        # Empty tree.
+        if root is None:
+            return 0
+        min_depth = 0
+        # # Minimum depth of left subtree.
+        left_depth = self.minDepth(root.left)
+        # Minimum depth of right subtree.
+        right_depth = self.minDepth(root.right)
+        # Minimum depth of the tree is the minimum depth of the left and right
+        # subtree plus 1 for the root.
+        if left_depth == 0 or right_depth == 0:
+            min_depth = left_depth + right_depth + 1
+        else:
+            min_depth = min(left_depth, right_depth) + 1
+        return min_depth
