cc7fd593fa40eb3b695d91c8a11448cfb2f5ca15
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-29 14:30:21 -0400

    Solve 100. Same Tree

--- easy/0100_same_tree.py
+++ easy/0100_same_tree.py
@@ -6,4 +6,13 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        
+        # Check if either tree is None.
+        if p is None or q is None:
+            # Two trees are the same if both are None.
+            return p == q
+        # Check if the values of both roots are the same.
+        if p.val != q.val:
+            return False
+        # Check if the left and right subtrees are the same.
+        return (self.isSameTree(p.left, q.left)
+                and self.isSameTree(p.right, q.right))
