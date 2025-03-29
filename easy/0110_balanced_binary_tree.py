208e99def20972738c6c8b29c0f48148970c77c5
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-29 15:32:37 -0400

    Solve 110. Balanced Binary Tree

--- easy/0110_balanced_binary_tree.py
+++ easy/0110_balanced_binary_tree.py
@@ -6,4 +6,23 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        
+        if root is None:
+            return True
+        return self.height(root) != -1
+    
+    def height(self, root):
+        if root is None:
+            return 0
+        # Height of left subtree.
+        left_height = self.height(root.left)
+        if left_height == -1:
+            return -1
+        # Height of right subtree.
+        right_height = self.height(root.right)
+        if right_height == -1:
+            return -1
+        # Check if the current tree is height-balanced.
+        if abs(left_height - right_height) > 1:
+            return -1
+        # Return the height of the current tree.
+        return max(left_height, right_height) + 1
