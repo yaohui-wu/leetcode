32e123209098fc35a061e222967819e7b06c3ea7
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-29 16:10:13 -0400

    Solve 112. Path Sum

--- easy/0112_path_sum.py
+++ easy/0112_path_sum.py
@@ -6,4 +6,11 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        
+        # Empty tree has no root-to-leaf paths.
+        if root is None:
+            return False
+        if root.left is None and root.right is None:
+            return root.val == targetSum
+        sum = targetSum - root.val
+        return (self.hasPathSum(root.left, sum)
+                or self.hasPathSum(root.right, sum))
