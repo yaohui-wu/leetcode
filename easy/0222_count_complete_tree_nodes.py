594afc291df755123bdfaa54bb1fc5f79682318c
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-29 17:30:37 -0400

    Solve 222. Count Complete Tree Nodes

--- easy/0222_count_complete_tree_nodes.py
+++ easy/0222_count_complete_tree_nodes.py
@@ -6,4 +6,22 @@
 #         self.right = right
 class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
-        
+        if root is None:
+            return 0
+        left_depth = self.depth(root.left)
+        right_depth = self.depth(root.right)
+        # Left subtree is a perfect binary tree.
+        # Right subtree is a complete binary tree.
+        if left_depth == right_depth:
+            count = 2 ** left_depth
+            return count + self.countNodes(root.right)
+        # Left subtree is a complete binary tree.
+        # Right subtree is a perfect binary tree.
+        count = 2 ** right_depth
+        return count + self.countNodes(root.left)
+    
+    def depth(self, root):
+        if root is None:
+            return 0
+        # The binary tree is complete.
+        return 1 + self.depth(root.left)
