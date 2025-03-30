3927bc3475b2253327a9b3bade141bbb4a326135
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-30 14:41:10 -0400

    Solve 225. Implement Stack using Queues

--- easy/0225_implement_stack_using_queues.py
+++ easy/0225_implement_stack_using_queues.py
@@ -1,19 +1,29 @@
+from collections import deque
+
 class MyStack:
 
     def __init__(self):
-        
+        self.queue = deque()
 
     def push(self, x: int) -> None:
-        
+        self.queue.append(x)
+        size = len(self.queue)
+        # Rotate the queue to maintain the stack order.
+        for _ in range(size - 1):
+            self.queue.append(self.queue.popleft())
 
     def pop(self) -> int:
-        
+        if self.empty():
+            return None
+        return self.queue.popleft()
 
     def top(self) -> int:
-        
+        if self.empty():
+            return None
+        return self.queue[0]
 
     def empty(self) -> bool:
-        
+        return len(self.queue) == 0
 
 
 # Your MyStack object will be instantiated and called as such:
