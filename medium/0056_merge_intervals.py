3ccc70097465b2e1ed0cf29a8e3c379d999b473c
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-03-31 17:40:34 -0400

    Solve 56. Merge Intervals

--- medium/0056_merge_intervals.py
+++ medium/0056_merge_intervals.py
@@ -1,3 +1,15 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        
+        length = len(intervals)
+        if length == 0 or length == 1:
+            return intervals
+        # Sort the intervals by the start of each interval.
+        intervals.sort(key=lambda interval: interval[0])
+        merged = [intervals[0]]
+        for interval in intervals:
+            # Combine the intervals if they overlap.
+            if interval[0] <= merged[-1][1]:
+                merged[-1][1] = max(merged[-1][1], interval[1])
+            else:
+                merged.append(interval)
+        return merged
