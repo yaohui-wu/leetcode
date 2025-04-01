35e97a9800ba155a99eac33f5f5f87f7adcb2e2f
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-04-01 19:32:01 -0400

    Solve 33. Search in Rotated Sorted Array

--- medium/0033_search_in_rotated_sorted_array.py
+++ medium/0033_search_in_rotated_sorted_array.py
@@ -1,3 +1,21 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        
+        # Binary search.
+        left = 0
+        right = len(nums)
+        while left < right:
+            mid = left + (right - left) // 2
+            num = nums[mid]
+            # Negative infinity case.
+            if target < nums[0] < num:
+                left = mid + 1
+            # Positive infinity case.
+            elif num < nums[0] <= target:
+                right = mid
+            elif num < target:
+                left = mid + 1
+            elif num > target:
+                right = mid
+            else:
+                return mid
+        return -1
