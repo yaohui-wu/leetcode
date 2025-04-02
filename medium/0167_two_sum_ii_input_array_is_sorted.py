a57ac858871dcee1fd00073641d93b92a4c404ac
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-04-02 14:40:49 -0400

    Solve 167. Two Sum II - Input Array Is Sorted

--- medium/0167_two_sum_ii_input_array_is_sorted.py
+++ medium/0167_two_sum_ii_input_array_is_sorted.py
@@ -1,3 +1,16 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        
+        left = 0
+        right = len(numbers) - 1
+        while left < right:
+            num = numbers[left] + numbers[right]
+            # Move left pointer to get a greater compliment.
+            if num < target:
+                left += 1
+            # Move right pointer to get a lesser compliment.
+            elif num > target:
+                right -= 1
+            else:
+                # Convert from 0-indexed to 1-indexed.
+                return [left + 1, right + 1]
+        return [0, 0]
