b1fe5a0ddf76fba08f43f8ca0a78ace72117839e
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-04-02 12:30:15 -0400

    Solve 34. Find First and Last Position of Element in Sorted Array

--- medium/0034_find_first_and_last_position_of-element_in_sorted_array.py
+++ medium/0034_find_first_and_last_position_of-element_in_sorted_array.py
@@ -1,3 +1,45 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        
+        length = len(nums)
+        if length == 0:
+            return [-1, -1]
+        if length == 1:
+            if nums[0] == target:
+                return [0, 0]
+            else:
+                return [-1, -1]
+        # Binary search to find the first and last occurrence of the target.
+        first = self.find_first(nums, target)
+        if first == -1:
+            return [-1, -1]
+        last = self.find_last(nums, target)
+        return [first, last]
+    
+    def find_first(self, nums, target):
+        left = 0
+        length = len(nums)
+        right = length - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        # Left is the first occurrence of the target if it exists.
+        if left < length and nums[left] == target:
+            return left
+        return -1
+    
+    def find_last(self, nums, target):
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] <= target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        # Right is the last occurrence of the target if it exists.
+        if right >= 0 and nums[right] == target:
+            return right
+        return -1
