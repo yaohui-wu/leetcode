8e7583cdfd921b1579c7e0d09eaa4bc7ec94565c
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2025-04-02 14:01:34 -0400

    Solve 240. Search a 2D Matrix II

--- medium/0240_search_a_2d_matrix_ii.py
+++ medium/0240_search_a_2d_matrix_ii.py
@@ -1,3 +1,18 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        
+        m = len(matrix) # Number of rows.
+        n = len(matrix[0]) # Number of columns.
+        # Start from the top-right corner.
+        row = 0
+        col = n - 1
+        while row < m and col >= 0:
+            num = matrix[row][col]
+            # Move down.
+            if num < target:
+                row += 1
+            # Move left.
+            elif num > target:
+                col -= 1
+            else:
+                return True
+        return False
